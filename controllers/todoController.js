050f9e7ffc97a3161e9f36612d202d1030834867
Desire <nbahbio@wgu.edu>
2018-02-16 01:51:09 -0500

    intitialized repository

---
+++ controllers/todoController.js
@@ -0,0 +1,22 @@
+module.exports = function(app) {
+    
+    app.get("/todo", (req,res) => {
+       res.render("todo.ejs");
+    });
+
+    app.post("/todo", (req,res) => {
+        
+    });
+
+    app.delete("/todo", (req,res) => {
+        
+    });
+
+    app.delete("/todo", (req,res) => {
+        
+    });
+
+    app.put("/todo", (req,res) => {
+       
+    });
+}
