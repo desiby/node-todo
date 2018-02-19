942d45e4946dbe441d128207d03ea10bb8e2fc2f
Desire <nbahbio@wgu.edu>
2018-02-19 00:27:30 -0500

    implemented POST and DELETE

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,24 @@
+const bodyParser = require("body-parser");
+var data = [{item: "get milk"}, {item: "do laundry"}, {item: "pay bills"}];
+//bodyparser middleware
+var urlEncodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
-    
+ 
     app.get("/todo", (req,res) => {
-       res.render("todo.ejs");
+       res.render("todo.ejs", {todos: data});
     });
 
-    app.post("/todo", (req,res) => {
-        
+    app.post("/todo", urlEncodedParser, (req,res) => {
+        data.push(req.body);
+        res.json(data);
     });
 
-    app.delete("/todo", (req,res) => {
-        
+    app.delete("/todo/:item", (req,res) => {
+        data = data.filter((todo) => {
+            return todo.item.replace(/ /g, "-") !== req.params.item;
+        });
+        res.json(data)
     });
 
     app.delete("/todo", (req,res) => {
